6e10467cc19be98865d37431d7859b13d65f4a68
Gabriela Dinica <dinica.gabriela@gmail.com>
2021-02-15 19:21:20 +0100

    Update install.sh
    Adding a stop before requesting the password to avoid timeout

--- Databases/timescaledb/install.sh
+++ Databases/timescaledb/install.sh
@@ -24,4 +24,8 @@ sudo -u postgres createdb $USER
 sudo -u postgres psql -c "ALTER USER $USER WITH SUPERUSER;"
 
 sudo -u postgres sh -c "echo \"shared_preload_libraries = 'timescaledb'\" >> /etc/postgresql/11/main/postgresql.conf"
+
+echo "Please be ready to input the password. Press [ENTER] to continue. If the password request times out, please run the following command separately \"sudo -u postgres /etc/init.d/postgresql restart\""
+read line
+
 sudo -u postgres /etc/init.d/postgresql restart
